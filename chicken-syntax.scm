99da7ae429d7928ee43f037a6c1440f7ced7d0c3
Evan Hanson <evhan@foldling.org>
2016-01-12 22:14:36 +1300

    Rehaul library loading
    Significantly changes the way modules and libraries are loaded.
    
    The `import` form now imports a module's identifiers *and* loads its
    implementation, making it similar to CHICKEN 4's `require-extension`.
    `require-extension` is now an alias for `import`.
    
    Syntax-only imports are provided by the new `import-syntax` form.
    `import-for-syntax` has been renamed accordingly.
    
    Import libraries now include a library identifier that is consulted in
    order to determine a module's presence. When loaded, a module provides
    this identifier via `##sys#provide` from the units unit.
    
    When shared libraries are loaded with `##sys#require`, either via
    `import` or some similar method, an attempt is made to load a toplevel
    corresponding to the required library's ID before falling back to the
    library's non-unit toplevel.
    
    Units specified by the "-uses" flag are treated identically to those
    declared used in-source.
    
    The "-R" flags to csc(1) and csi(1) now use `import` to load the
    requested libraries.

--- chicken-syntax.scm
+++ chicken-syntax.scm
@@ -1081,7 +1081,7 @@
  (##sys#er-transformer
   (lambda (x r c)
     (##sys#check-syntax 'use x '(_ . #(_ 0)))
-    `(##core#require-extension ,(cdr x) #t))))
+    `(,(r 'require-extension) ,@(cdr x)))))
 
 (##sys#extend-macro-environment
  'use-for-syntax '()
