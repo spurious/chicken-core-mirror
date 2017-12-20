1961495d413a431b13ce0ce28a59dc2b804b596a
felix <felix@call-with-current-continuation.org>
2017-12-20 23:00:30 +0100

    fixes in clean.sh

--- clean.sh
+++ clean.sh
@@ -36,6 +36,7 @@ usage () {
 confclean=
 testclean=
 clean=1
+spotless=
 
 case "$1" in
     --?help|--?h)
@@ -46,9 +47,10 @@ case "$1" in
         clean=
         testclean=1;;
     -spotless)
+        spotless=1
         confclean=1
         testclean=1;;
-    *)
+    -*)
         usage 1;;
 esac
 
@@ -61,10 +63,13 @@ if test -n "${clean}"; then
     rm -f ${PROGRAM_PREFIX}chicken-status${PROGRAM_SUFFIX}${EXE}
     rm -f ${PROGRAM_PREFIX}chicken-propile${PROGRAM_SUFFIX}${EXE}
     rm -f ${PROGRAM_PREFIX}feathers${PROGRAM_SUFFIX}${EXE}
-    rm -f lib${PROGRAM_PREFIX}chicken${PROGRAM_SUFFIX}${DYLIB}*
-    rm -fr *.import.so
+    rm -f lib${PROGRAM_PREFIX}chicken${PROGRAM_SUFFIX}${DYLIB}
+    rm -f lib${PROGRAM_PREFIX}chicken${PROGRAM_SUFFIX}${DYLIB}.${BINARYVERSION}
+    rm -f lib${PROGRAM_PREFIX}chicken${PROGRAM_SUFFIX}${A}
+    rm -f *.import.so
+    rm -f *.o
     rm -f srfi-4.import.scm
-    for x in bitwise blob errno file.posix fixnum flonum format gc io keyword load locative memory	memory.representation platform plist posix pretty-print process process.signal process-context random syntax	sort string time time.posix continuation data-structures eval file internal irregex pathname port read-syntax repl tcp; do
+    for x in bitwise blob errno file.posix fixnum flonum format gc io keyword load locative memory	memory.representation platform plist posix pretty-print process process.signal process-context random sort string time time.posix continuation data-structures eval file internal irregex pathname port read-syntax repl tcp; do
         rm -f chicken.${x}.import.scm
     done
     rm -f chicken.compiler.user-pass.import.scm
@@ -81,11 +86,26 @@ if test -n "${testclean}"; then
     rm -f ${SRCDIR}/tests/*.out
     rm -f ${SRCDIR}/tests/*.profile
     rm -f ${SRCDIR}/tests/*.so
-    rm -f ${SRCDIR}/tests/tmp*
+    rm -fr ${SRCDIR}/tests/tmp*
     rm -f ${SRCDIR}/tests/empty-file
     rm -f ${SRCDIR}/tests/null
     rm -f ${SRCDIR}/tests/null.c
     rm -f ${SRCDIR}/tests/null.o
     rm -f ${SRCDIR}/tests/null.exe
     rm -fr ${SRCDIR}/tests/test-repository
+fi
+
+if test -n "${spotless}"; then
+    rm -f buildid buildbranch
+    rm -f chicken.compiler.*.import.scm
+    rm -f batch-driver.c build-version.c c-backend.c chicken.c
+    rm -f chicken-ffi-syntax.c chicken-install.c chicken-profile.c
+    rm -f chicken-status.c chicken-syntax.c chicken-uninstall.c
+    rm -f compiler-syntax.c continuation.c core.c c-platform.c
+    rm -f csc.c csi.c data-structures.c debugger-client.c eval.c
+    rm -f expand.c extras.c file.c files.c internal.c irregex.c
+    rm -f lfa2.c library.c lolevel.c modules.c optimizer.c pathname.c
+    rm -f port.c ports.c posixunix.c profiler.c read-syntax.c repl.c
+    rm -f scheduler.c scrutinizer.c srfi-4.c stub.c
+    rm -f support.c tcp.c user-pass.c
 fi
