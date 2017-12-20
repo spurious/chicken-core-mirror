e1a5dfcf3d34f7fa73413c0fc6b13d85a30fa586
felix <felix@call-with-current-continuation.org>
2017-12-20 23:01:11 +0100

    first sort of working linux build

--- dependencies.scm
+++ dependencies.scm
@@ -1,4 +1,27 @@
 ;;;; build dependency tree
+;
+; Copyright (c) 2017, The CHICKEN Team
+; All rights reserved.
+;
+; Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
+; conditions are met:
+;
+;   Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+;     disclaimer. 
+;   Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+;     disclaimer in the documentation and/or other materials provided with the distribution. 
+;   Neither the name of the author nor the names of its contributors may be used to endorse or promote
+;     products derived from this software without specific prior written permission. 
+;
+; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+; OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+; AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+; CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+; SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+; THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+; OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+; POSSIBILITY OF SUCH DAMAGE.
 
 
 ;;; leaf objects
@@ -35,12 +58,26 @@
     build-version))
 
 (define compiler-objects
-  '(chicken batch-driver core optimizer lfa2 compiler-syntax
-            scrutinizer support c-platform user-pass))
+  '(chicken 
+    batch-driver
+    core
+    optimizer
+    lfa2
+    compiler-syntax
+    scrutinizer
+    support
+    c-backend
+    c-platform
+    user-pass))
 
 (define programs 
-  '(chicken csc csi chicken-install chicken-uninstall
-            chicken-status chicken-profile))
+  '(chicken
+    csc
+    csi
+    chicken-install
+    chicken-uninstall
+    chicken-status
+    chicken-profile))
 
 (define c-files '(dbg-stub runtime))
 
@@ -123,7 +160,6 @@
     chicken.gc
     chicken.io
     chicken.keyword
-    chicken.import
     chicken.load 
     chicken.locative
     chicken.memory
@@ -144,7 +180,6 @@
     chicken.data-structures
     chicken.eval
     chicken.file
-    chicken.internal 
     chicken.irregex
     chicken.pathname
     chicken.port
@@ -178,19 +213,13 @@
           (map (o o-file rc-file) resource-files)
           (so-file (import-library import-libraries))))
 
-;;XXX manpages for programs (mangled) + feathers
-
 ;;XXX distfiles (generate here or separate script?)
-
-;;XXX platform/arch-specific compiler options
 
 ;;XXX map symbolic names to composite names that are 
 ;;    parameterized/mangled (including "cyg...")
 
 ;;XXX distfiles
 
-;;XXX USES_SONAME
-;;XXX relinking
 ;;XXX cygwin specifics
 
 (abstract 'primary-libchicken)
@@ -199,10 +228,10 @@
   libchicken.a libchicken.so)
 
 (depends libchicken.a 
-  ,(static-o-file libchicken-objects))
+  ,(static-o-file (cons 'runtime libchicken-objects)))
 
 (depends libchicken.so
-  ,(o-file libchicken-objects))
+  ,(o-file (cons 'runtime libchicken-objects)))
     
 (for-each
   (lambda (prg)
@@ -253,51 +282,6 @@
 
 (depends chicken-uninstall 
   ,(o-file (rc-file 'chicken-uninstall)))
-
-(depends ,(map (o c-file import-library)
-            '(chicken.posix
-              chicken.errno
-              chicken.process
-              chicken.process.signal
-              chicken.file.posix))
-  ,(scm-file (if WINDOWS 'posixwin 'posixunix)))
-
-(depends ,(map (o c-file import-library)
-            '(chicken.bitwise
-              chicken.blob
-              chicken.fixnum
-              chicken.flonum
-              chicken.gc
-              chicken.keyword
-              chicken.platform
-              chicken.plist
-              chicken.time))
-  ,(scm-file 'library))
-
-(depends ,(c-file (import-library 'chicken.load))
-  ,(scm-file 'eval))
-
-(depends ,(c-file (import-library 'chicken.syntax))
-  ,(scm-file 'expand))
-
-(depends ,(map (o c-file import-library)
-            '(chicken.format
-              chicken.io
-              chicken.pretty-print
-              chicken.random))
-  ,(scm-file 'extras))
-
-(depends ,(map (o c-file import-library)
-            '(chicken.sort
-              chicken.string))
-  ,(scm-file 'data-structures))
-
-(depends ,(map (o c-file import-library)
-            '(chicken.locative
-              chicken.memory
-              chicken.memory.representation
-              chicken.random))
-  ,(scm-file 'lolevel))
 
 (depends chicken.c
   mini-srfi-1.scm
@@ -373,7 +357,9 @@
   chicken.sort.import.scm 
   chicken.string.import.scm)
 
-(depends scheduler.c	chicken.format.import.scm)
+(depends scheduler.c	
+  common-declarations.scm
+  chicken.format.import.scm)
 
 (depends scrutinizer.c
   mini-srfi-1.scm
@@ -402,6 +388,8 @@
   chicken.format.import.scm)
 
 (depends chicken-ffi-syntax.c
+  common-declarations.scm
+  mini-srfi-1.scm
   chicken.format.import.scm 
   chicken.internal.import.scm 
   chicken.string.import.scm)
@@ -430,6 +418,8 @@
   chicken.time.import.scm)
 
 (depends modules.c
+  common-declarations.scm
+  mini-srfi-1.scm
   chicken.internal.import.scm 
   chicken.keyword.import.scm 
   chicken.load.import.scm 
@@ -508,7 +498,7 @@
 
 (depends chicken-uninstall.c
   mini-srfi-1.scm 
-  egg-environment-scm 
+  egg-environment.scm 
   egg-information.scm
   chicken.file.import.scm 
   chicken.foreign.import.scm 
@@ -520,10 +510,13 @@
   chicken.string.import.scm)
 
 (depends chicken-syntax.c
+  common-declarations.scm
+  mini-srfi-1.scm
   chicken.platform.import.scm 
   chicken.internal.import.scm)
 
 (depends srfi-4.c
+  common-declarations.scm
   chicken.bitwise.import.scm 
   chicken.foreign.import.scm 
   chicken.gc.import.scm 
@@ -531,6 +524,9 @@
   chicken.syntax.import.scm)
 
 (depends posixunix.c
+  posix-common.scm
+  posixunix.scm
+  common-declarations.scm
   chicken.bitwise.import.scm 
   chicken.condition.import.scm 
   chicken.foreign.import.scm 
@@ -541,6 +537,9 @@
   chicken.time.import.scm)
 
 (depends posixwin.c
+  posix-common.scm
+  posixwin.scm
+  common-declarations.scm
   chicken.condition.import.scm 
   chicken.bitwise.import.scm 
   chicken.foreign.import.scm 
@@ -552,6 +551,7 @@
   chicken.time.import.scm)
 
 (depends data-structures.c
+  common-declarations.scm
   chicken.condition.import.scm 
   chicken.foreign.import.scm)
 
@@ -565,6 +565,7 @@
   chicken.internal.import.scm)
 
 (depends extras.c
+  common-declarations.scm
   chicken.string.import.scm 
   chicken.time.import.scm)
 
@@ -580,38 +581,123 @@
   chicken.platform.import.scm 
   chicken.syntax.import.scm)
 
-(depends irregex.c chicken.syntax.import.scm)
+(depends irregex.c 
+  irregex-core.scm
+  irregex-utils.scm
+  common-declarations.scm
+  chicken.syntax.import.scm)
 
-(depends repl.c	chicken.eval.import.scm)
+(depends repl.c	
+  common-declarations.scm
+  chicken.eval.import.scm)
+
+(depends stub.c common-declarations.scm)
+(depends profiler.c common-declarations.scm)
+(depends pathname.c common-declarations.scm)
+
+(depends debugger-client.c 
+  common-declarations.scm
+  dbg-stub.c)
 
 (depends file.c
+  common-declarations.scm
   chicken.io.import.scm 
   chicken.irregex.import.scm 
   chicken.foreign.import.scm 
   chicken.pathname.import.scm 
   chicken.posix.import.scm)
 
-(depends lolevel.c	chicken.foreign.import.scm)
+(depends lolevel.c	
+  common-declarations.scm
+  chicken.foreign.import.scm)
 
 (depends pathname.c
+  common-declarations.scm
   chicken.irregex.import.scm 
   chicken.platform.import.scm 
   chicken.string.import.scm)
 
-(depends port.c	chicken.io.import.scm)
+(depends port.c	
+  common-declarations.scm
+  chicken.io.import.scm)
 
 (depends read-syntax.c
   chicken.internal.import.scm 
   chicken.platform.import.scm)
 
 (depends tcp.c
+  common-declarations.scm
   chicken.foreign.import.scm 
   chicken.port.import.scm 
   chicken.time.import.scm)
 
-(depends internal.c mini-srfi-1.scm)
+(depends continuation.c common-declarations.scm)
 
+(depends internal.c mini-srfi-1.scm)
 (depends read-syntax.c common-declarations.scm)
+
+(depends chicken.compiler.user-pass.import.c user-pass.c)
+(depends srfi-4.import.c srfi-4.c)
+
+(depends ,(scm-file (import-library '(chicken.bitwise
+                                    chicken.fixnum
+                                    chicken.flonum
+                                    chicken.gc
+                                    chicken.keyword
+                                    chicken.blob
+                                    chicken.platform 
+                                    chicken.plist
+                                    chicken.time)))
+  library.c)
+
+(depends ,(scm-file (import-library '(chicken.file.posix
+                                    chicken.posix 
+                                    chicken.process 
+                                    chicken.process.signal
+                                    chicken.process-context
+                                    chicken.time.posix
+                                    chicken.errno)))
+  ,(if WINDOWS 'posixwin.c 'posixunix.c))
+
+(depends ,(scm-file (import-library '(chicken.format
+                                    chicken.io
+                                    chicken.pretty-print
+                                    chicken.random )))
+  extras.c)
+
+(depends chicken.internal.import.scm internal.c)
+
+(depends (chicken.eval.import.scm chicken-load.import.scm)
+  eval.c)
+
+(depends (chicken.locative.import.scm
+          chicken.memory.import.scm
+          chicken.memory.representation.import.scm)
+  lolevel.c)
+
+(depends (chicken.sort.import.scm
+          chicken.string.import.scm
+          chicken.data-structures.scm)
+  data-structures.c)
+
+(depends chicken.continuation.import.scm continuation.c)
+(depends chicken.file.import.scm file.c)
+(depends chicken.irregex.import.scm irregex.c)
+(depends chicken.pathname.import.scm pathname.c)
+(depends chicken.port.import.scm port.c)
+(depends chicken.read-syntax.import.scm read-syntax.c)
+(depends chicken.repl.import.scm repl.c)
+(depends chicken.tcp.import.scm tcp.c)
+(depends chicken.compiler.support.scm support.c)
+(depends chicken.compiler.compiler-syntax.import.scm compiler-syntax.c)
+(depends chicken.compiler.core.import.scm core.c)
+(depends chicken.compiler.batch-driver.import.scm batch-driver.c)
+(depends chicken.compiler.optimizer.import.scm optimizer.c)
+(depends chicken.compiler.scrutinizer.import.scm scrutinizer.c)
+(depends chicken.compiler.lfa2.import.scm lfa2.c)
+(depends chicken.compiler.c-platform.import.scm c-platform.c)
+(depends chicken.compiler.c-backend.import.scm c-backend.c)
+(depends chicken.compiler.user-pass.import.scm user-pass.c)
 
 
 ;;XXX windows implib (.dll.a)
@@ -622,34 +708,44 @@
 ;;XXX compiler, with differing options
 
 (ld 'libchicken.so
-  (map o-file libchicken-objects))
+  (map o-file (cons 'runtime libchicken-objects)))
 
 (static-ld 'libchicken.a
-  (map static-o-file libchicken-objects))
+  (map static-o-file (cons 'runtime libchicken-objects)))
 
 (for-each (cut rc <>) resource-files)
 
 (unless WINDOWS
   (symlink 'libchicken.so.link 'libchicken.so))
 
+(define (sans lst . xs)
+  (remove (cut memq <> xs) lst))
+
 (for-each
   (lambda (p)
     (ld p (list (o-file p) 'primary-libchicken)))
-  programs)
+  (sans programs 'chicken))
+
+(ld 'chicken 
+  (append (o-file compiler-objects) '(primary-libchicken)))
 
 (for-each
   (lambda (f)
     (cc f)
     (cc f (static-o-file f)))
-  (append (remove (cut eq? <> 'chicken) programs)
+  (append (sans programs 'chicken)
+          '(runtime)
           libchicken-objects
           compiler-objects))
 
 (for-each chicken
-  (append (remove (cut eq? <> 'chicken) programs)
+  (append (sans programs 'chicken)
           compiler-objects
           (import-library import-libraries)
           libchicken-objects))
+
+(put! 'posixunix.c 'source-file 'posix)
+(put! 'posixwin.c 'source-file 'posix)
 
 (construct 'feathers 'feathers.in)
 
@@ -712,7 +808,67 @@
 (chicken-options read-syntax
   "-emit-import-library" "chicken.read-syntax")
 
+(chicken-options file
+  "-emit-import-library" "chicken.file")
+
+(chicken-options port
+  "-emit-import-library" "chicken.port")
+
 (chicken-options expand "-no-module-registration")
+
+(chicken-options irregex
+  "-emit-import-library" "chicken.irregex")
+
+(chicken-options extras
+  "-emit-import-library" "chicken.format"
+  "-emit-import-library" "chicken.io"
+  "-emit-import-library" "chicken.pretty-print"
+  "-emit-import-library" "chicken.random")  
+
+(chicken-options posixunix
+  "-feature" "platform-unix"
+  "-emit-import-library" "chicken.errno"
+  "-emit-import-library" "chicken.file.posix"
+  "-emit-import-library" "chicken.time.posix"
+  "-emit-import-library" "chicken.process"
+  "-emit-import-library" "chicken.process.signal"  
+  "-emit-import-library" "chicken.process-context"  
+  "-emit-import-library" "chicken.posix")   
+
+(chicken-options posixunix
+  "-feature" "platform-windows"
+  "-emit-import-library" "chicken.errno"
+  "-emit-import-library" "chicken.file.posix"
+  "-emit-import-library" "chicken.time.posix"
+  "-emit-import-library" "chicken.process"
+  "-emit-import-library" "chicken.process.signal"  
+  "-emit-import-library" "chicken.process-context"  
+  "-emit-import-library" "chicken.posix")   
+
+(chicken-options continuation
+  "-emit-import-library" "chicken.continuation")
+
+(chicken-options data-structures
+  "-emit-import-library" "chicken.data-structures"
+  "-emit-import-library" "chicken.sort"
+  "-emit-import-library" "chicken.string")
+
+(chicken-options pathname
+  "-emit-import-library" "chicken.pathname")
+
+(chicken-options tcp
+  "-emit-import-library" "chicken.tcp")
+
+(chicken-options lolevel
+  "-emit-import-library" "chicken.locative"
+  "-emit-import-library" "chicken.memory"
+  "-emit-import-library" "chicken.memory.representation")
+
+(chicken-options pathname
+  "-emit-import-library" "chicken.pathname")
+
+(chicken-options srfi-4
+  "-emit-import-library" "srfi-4")
 
 (chicken-options ,programs
   "-no-lambda-info" #(EXTRA_CHICKEN_PROGRAM_OPTIONS))
@@ -721,13 +877,24 @@
   "-feature" "chicken-compile-shared" "-dynamic" 
   "-no-trace")
 
-(ld-options libchicken-so
+(for-each
+  (lambda (cf)
+    (chicken-options ,cf
+                     "-emit-import-library"
+                     ,(symbol-append 'chicken.compiler. cf)))
+  '(batch-driver c-backend compiler-syntax
+    core c-platform lfa2 optimizer scrutinizer support
+    user-pass))
+
+(ld-options libchicken.so
   #(LINKER_LINK_SHARED_LIBRARY_OPTIONS)
+  #(LIBCHICKEN_SO_LINKER_OPTIONS)
   #(LIBRARIES))
 
 (ld-options ,programs
   #(LINKER_LINK_SHARED_PROGRAM_OPTIONS)
-  #(STATIC_EXTRALIBS))
+  "-lchicken"
+  #(LIBRARIES))
 
 (ld-options chicken-install #(CHICKEN_INSTALL_RC_O))
 (ld-options chicken-uninstall #(CHICKEN_UNINSTALL_RC_O))
@@ -735,13 +902,17 @@
 (ld-options ,(so-file (import-library import-libraries))
   "-DC_SHARED" #(LINKER_LINK_SHARED_DLOADABLE_OPTIONS))
 
-(cc-options ,(append (o-file libchicken-objects)
-                     (static-o-file libchicken-objects))
+(cc-options ,(append (o-file (cons 'runtime 
+                                   libchicken-objects))
+                     (static-o-file (cons 'runtime 
+                                          libchicken-objects)))
   "-DC_BUILDING_LIBCHICKEN")
 
-;;XXX install
-;;XXX uninstall
-    
+(cc-options ,(o-file (append (cons 'runtime 
+                                   libchicken-objects)
+                             (import-library import-libraries)))
+  #(C_COMPILER_SHARED_OPTIONS))
+
 
 ;; name mapping
 
@@ -771,5 +942,7 @@
     ,@(map (lambda (f) (list f `(#(SRCDIR) / ,f)))
         (append (map c-file c-files) 
                 (map rc-file resource-files)
-                (map scm-file scheme-files)))
+                (map scm-file scheme-files)
+                (map (o scm-file import-library) 
+                  primitive-import-libraries)))
     ))
--- mkbuild.scm
+++ mkbuild.scm
@@ -1,12 +1,41 @@
 ;;;; build-script generator
+;
+; Copyright (c) 2017, The CHICKEN Team
+; All rights reserved.
+;
+; Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following
+; conditions are met:
+;
+;   Redistributions of source code must retain the above copyright notice, this list of conditions and the following
+;     disclaimer. 
+;   Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following
+;     disclaimer in the documentation and/or other materials provided with the distribution. 
+;   Neither the name of the author nor the names of its contributors may be used to endorse or promote
+;     products derived from this software without specific prior written permission. 
+;
+; THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS
+; OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+; AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDERS OR
+; CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+; CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+; SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+; THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+; OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+; POSSIBILITY OF SUCH DAMAGE.
 
-(import (chicken) (chicken data-structures)
+
+(import (chicken data-structures)
         (chicken plist) (chicken sort)
         (chicken pretty-print) (chicken format)
         (chicken port) (chicken string))
 
 
 (include "mini-srfi-1.scm")
+
+(define show-dag #f)
+(define show-order #f)
+(define gen-dot #f)
+(define dot-targets '())
 
 
 ;; utilities
@@ -61,29 +90,26 @@
 (define default-chicken-options '())
 (define default-ld-options '())
 
-(define (add-options t opts prop defaults)
+(define (add-options t opts prop)
   (for-each
     (lambda (t)
-      (put! t prop (append (get t prop defaults) opts)))
+      (put! t prop (append (get t prop '()) opts)))
     (if (list? t) t (list t))))
 
 (define-syntax cc-options
   (syntax-rules ()
     ((_ t opts ...)
-     (add-options `t `(opts ...) 'cc-options 
-                  default-cc-options))))
+     (add-options `t `(opts ...) 'cc-options))))
 
 (define-syntax chicken-options
   (syntax-rules ()
     ((_ t opts ...)
-     (add-options `t `(opts ...) 'chicken-options
-                  default-chicken-options))))
+     (add-options `t `(opts ...) 'chicken-options))))
 
 (define-syntax ld-options
   (syntax-rules ()
     ((_ t opts ...)
-     (add-options `t `(opts ...) 'ld-options
-                  default-ld-options))))
+     (add-options `t `(opts ...) 'ld-options))))
 
 
 ;;; Build commands
@@ -103,14 +129,14 @@
   (build outf
          (lambda ()
            (let ((cf (c-file f))
-                 (opts (get f 'cc-options
-                            default-cc-options)))
+                 (opts (append (get outf 'cc-options '())
+                               default-cc-options)))
              (if WINDOWS
-                 (printf "%C_COMPILER% ~a -c -o ~a ~a" 
+                 (printf "%C_COMPILER% ~a -c ~a -o ~a" 
                          (join opts)
                          (real-name cf)
                          (real-name outf))
-                 (printf "${C_COMPILER} ~a -c -o ~a ~a"
+                 (printf "${C_COMPILER} ~a -c ~a -o ~a"
                          (join opts)
                          (real-name cf)
                          (real-name outf)))))))
@@ -129,29 +155,30 @@
 (define (chicken f #!optional (outf (c-file f)))
   (build outf 
          (lambda ()
-           (let ((opts (get f 'chicken-options 
-                            default-chicken-options)))
+           (let ((opts (append (get f 'chicken-options '())
+                               default-chicken-options))
+                 (src (get outf 'source-file f)))
              (if WINDOWS
                  (printf "%CHICKEN% ~a -output-file ~a ~a" 
-                         (real-name (scm-file f))
+                         (real-name (scm-file src))
                          (real-name outf)
                          (join opts))
                  (printf "${CHICKEN} ~a -output-file ~a ~a"
-                         (real-name (scm-file f))
+                         (real-name (scm-file src))
                          (real-name outf)
                          (join opts)))))))
 
 (define (ld lib fs)
   (build lib
          (lambda ()
-           (let ((opts (get lib 'ld-options 
-                            default-ld-options)))
+           (let ((opts (append (get lib 'ld-options '())
+                               default-ld-options)))
              (printf (if WINDOWS 
-                         "%LINKER% ~a ~a -o ~a"
-                         "${LINKER} ~a ~a -o ~a")
-                     (join opts)
+                         "%LINKER% ~a -o ~a ~a"
+                         "${LINKER} ~a -o ~a ~a")
                      (join (map real-name fs))
-                     (real-name lib))))))
+                     (real-name lib)
+                     (join opts))))))
 
 (define (static-ld lib fs)
   (build lib
@@ -160,8 +187,7 @@
                        "%LIBRARIAN% %LIBRARIAN_OPTIONS% ~a ~a"
                        "${LIBRARIAN} ${LIBRARIAN_OPTIONS} ~a ~a")
                    (real-name lib)
-                   (join (map (o real-name static-o-file) 
-                           fs))))))
+                   (join (map real-name fs))))))
 
 (define (symlink to from)
   (build to
@@ -265,32 +291,64 @@
       (when (memq t seen)
         (error "circular dependency" t seen))
       (let ((deps (get t 'depends '())))
-        (set! dag (cons (cons t deps) dag))
+        (cond ((assq t dag) =>
+               (lambda (a)
+                 (set-cdr! a (lset-union/eq? (cdr a) deps))))
+              (else
+                (set! dag (cons (cons t deps) dag))))
         (let ((seen (cons t seen)))
           (for-each (cut gather <> seen) deps))))
     (for-each (cut gather <> '()) toplevel-targets)
+    (when show-dag
+      (pp dag)
+      (exit))
+    (when gen-dot
+      (dot dag)
+      (exit))
     (reverse (topological-sort dag eq?))))
 
+(define (dot dag)
+  (with-output-to-file "chicken.dot"
+    (lambda ()
+      (print "digraph chicken {")
+      (for-each
+        (lambda (edges)
+          (let ((n (car edges)))
+            (when (or (null? dot-targets)
+                      (memq n dot-targets))
+              (for-each
+                (lambda (edge)
+                  (printf "  \"~a\" -> \"~a\";~%" n edge))
+                (cdr edges)))))
+        dag)
+      (print "}"))))
+
 (define (emit-build-script name)
-  (let ((dag (sort-targets))
-        (out (open-output-file name #:binary)))
-    (when WINDOWS (set! nl "\r\n"))
-    (if WINDOWS
-        (display "@echo off\r\nrem GENERATED BY mkbuild.scm\r\ncall config.bat\t\nif %SRCDIR == \"\" set SRCDIR=.\r\"\r\n" out))
-        (display "#!/bin/sh\n# GENERATED BY mkbuild.scm\nset -e\n. config.sh\nif test \"${SRCDIR}\" = \"\"; then\n  SRCDIR=.\nfi\n" out)
-    (for-each
-      (lambda (t)
-        (let ((cmd (get t 'command)))
-          (when cmd
-            (emit-build-rule t
-                             (get t 'command) 
-                             (get t 'depends '())
-                             out))))
-      dag)
-    (if WINDOWS
-        (display "rem END OF GENERATED FILE\r\n" out)
-        (display "# END OF GENERATED FILE\n" out))
-    (close-output-port out)))
+  (print "sorting targets ...")
+  (flush-output)
+  (let ((order (sort-targets)))
+    (when show-order
+      (pp order)
+      (exit))
+    (print "generating build script " name " ...")
+    (let ((out (open-output-file name #:binary)))
+      (when WINDOWS (set! nl "\r\n"))
+      (if WINDOWS
+          (display "@echo off\r\nrem GENERATED BY mkbuild.scm\r\ncall config.bat\t\nif %SRCDIR == \"\" set SRCDIR=.\r\"\r\n" out)
+          (display "#!/bin/sh\n# GENERATED BY mkbuild.scm\nset -e\n. config.sh\nif test \"${SRCDIR}\" = \"\"; then\n  SRCDIR=.\nfi\n" out))
+      (for-each
+        (lambda (t)
+          (let ((cmd (get t 'command)))
+            (when cmd
+              (emit-build-rule t
+                               (get t 'command) 
+                               (get t 'depends '())
+                               out))))
+        order)
+      (if WINDOWS
+          (display "rem END OF GENERATED FILE\r\n" out)
+          (display "# END OF GENERATED FILE\n" out))
+      (close-output-port out))))
 
 (define (emit-build-rule t cmd deps out)
   (unless (get t 'abstract)
@@ -324,6 +382,10 @@
 
 ;; command line
 
+(define (usage #!optional (status 1))
+  (display "usage: mkbuild.scm [-help] [-dag] [-order] [-windows] [-unix] [-dot] [-target TARGET]\n" (current-error-port))
+  (exit status))
+
 (define (mkbuild args)
   (let loop ((args args))
     (unless (null? args)
@@ -332,15 +394,32 @@
         (cond ((string=? "-windows" arg)
                (set! WINDOWS #t)
                (loop rest))
+              ((string=? "-dag" arg)
+               (set! show-dag #t)
+               (loop rest))
+              ((member arg '("-h" "-help" "--help"))
+               (usage 0))
+              ((string=? "-order" arg)
+               (set! show-order #t)
+               (loop rest))
+              ((string=? "-dot" arg)
+               (set! gen-dot #t)
+               (loop rest))
+              ((string=? "-target" arg)
+               (set! dot-targets 
+                 (cons (string->symbol (catr args) dot-targets)))
+               (loop (cdr args)))
               ((string=? "-unix" arg)
                (set! WINDOWS #f)
                (loop rest))
-              (else (error "unknown option" arg)))))
-    (load "dependencies.scm")
-    (set! name-map (build-name-map))
-    (if WINDOWS
-        (emit-build-script "build.bat")
-        (emit-build-script "build.sh"))))
+              (else (usage))))))
+  (print "loading definitions ...")
+  (flush-output)
+  (load "dependencies.scm")
+  (set! name-map (build-name-map))
+  (if WINDOWS
+      (emit-build-script "build.bat")
+      (emit-build-script "build.sh")))
 
 
 (mkbuild (command-line-arguments))
