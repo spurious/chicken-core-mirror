3254eb3ad5cd9fb0c6fa0227e916220b477a3fc9
Peter Bex <peter@more-magic.net>
2022-08-07 15:56:33 +0200

    Update bootstrap script to use CHICKEN 5.3.0
    The 5.2.0 release has a couple annoying known bugs.  And generally,
     it's better to bootstrap with the latest stable release anyway.

--- scripts/bootstrap.sh
+++ scripts/bootstrap.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-# build 5.2.0 release
+# build 5.3.0 release
 
 mkdir -p boot/snapshot
 cd boot
-wget -c https://code.call-cc.org/releases/5.2.0/chicken-5.2.0.tar.gz
-tar -xzf chicken-5.2.0.tar.gz
-cd chicken-5.2.0
+wget -c https://code.call-cc.org/releases/5.3.0/chicken-5.3.0.tar.gz
+tar -xzf chicken-5.3.0.tar.gz
+cd chicken-5.3.0
 make "$@" PREFIX="$(pwd)"/../snapshot
 make "$@" PREFIX="$(pwd)"/../snapshot install
 cd ../..
@@ -21,8 +21,8 @@ make "$@" CHICKEN="$(pwd)"/boot/snapshot/bin/chicken boot-chicken
 
 # remove snapshot installation and tarball
 rm -fr boot/snapshot
-rm -fr boot/chicken-5.2.0
-rm -f  boot/chicken-5.2.0.tar.gz
+rm -fr boot/chicken-5.3.0
+rm -f  boot/chicken-5.3.0.tar.gz
 
 echo
 echo 'Now, build chicken by passing "CHICKEN=./chicken-boot" to make,'
